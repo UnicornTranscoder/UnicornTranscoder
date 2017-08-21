4be0d8eaeece2b981c38dff7cc0164a6164b8421
Benjamin DROUARD <benjamin.drouard@epitech.eu>
2017-08-21 17:27:23 +0200

    Template m3u8

--- core/m3u8.js
+++ core/m3u8.js
@@ -11,7 +11,8 @@ let m3u8 = {};
 
 m3u8.serve = function (req, res) {
     debug('M3U8 ' + req.params.sessionId);
-    universal.cache[req.params.sessionId] = new Transcoder(req.params.sessionId, req, res);
+    universal.cache[req.params.sessionId] = new Transcoder(req.params.sessionId, req);
+    res.sendFile(config.template_directory + 'index.m3u8');
 };
 
 m3u8.serveChunk = function (req, res) {
