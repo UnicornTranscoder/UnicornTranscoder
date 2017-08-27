732bf8f4de78759a0e3650492fa3e37b758c0483
Benjamin DROUARD <benjamin.drouard@epitech.eu>
2017-08-27 19:05:45 +0200

    Redis polling dash chunks

--- core/dash.js
+++ core/dash.js
@@ -17,62 +17,36 @@ dash.serve = function (req, res) {
 };
 
 dash.serveInit = function (req, res) {
-
-    // Allow CORS
+    let sessionId = req.params.sessionId;
     res.header('Access-Control-Allow-Origin', '*');
 
-    let count = 0;
-    let sessionId = req.params.sessionId;
-
-    function doWork() {
-        if (!req.connection.destroyed) {
-            if (fs.existsSync(config.xdg_cache_home + sessionId + "/init-stream" + req.params.streamId + ".m4s")) {
-                debug('Serving init-stream' + req.params.streamId + '.m4s for session ' + sessionId);
-                res.download(config.xdg_cache_home + sessionId + "/init-stream" + req.params.streamId + ".m4s");
-            } else {
-                if (count < 20) {
-                    count++;
-                    setTimeout(doWork, 1000);
-                } else {
-                    res.status(404).send('Not found');
-                }
-            }
-        }
-    }
-    doWork();
-
     if ((typeof universal.cache[sessionId]) != 'undefined' && universal.cache[sessionId].alive == true) {
-        universal.updateTimeout(sessionId);
+        universal.cache[sessionId].getChunk(0, () => {
+            debug('Serving init-stream' + req.params.streamId + '.m4s for session ' + sessionId);
+            res.sendFile(config.xdg_cache_home + sessionId + "/init-stream" + req.params.streamId + ".m4s");
+
+            universal.updateTimeout(sessionId);
+        }, req.params.streamId)
+    } else {
+        debug(req.params.sessionId + ' not found');
+        res.status(404).send('Session not found');
     }
 };
 
 dash.serveChunk = function (req, res) {
-
-    // Allow CORS
+    let sessionId = req.params.sessionId;
     res.header('Access-Control-Allow-Origin', '*');
 
-    let count = 0;
-    let sessionId = req.params.sessionId;
-
-    function doWork() {
-        if (!req.connection.destroyed) {
-            if (fs.existsSync(config.xdg_cache_home + sessionId + "/chunk-stream" + req.params.streamId + "-" + utils.pad(parseInt(req.params.partId) + 1, 5) + ".m4s")) {
-                debug('Serving chunk-stream' + req.params.streamId + "-" + utils.pad(parseInt(req.params.partId) + 1, 5) + '.m4s for session ' + sessionId);
-                res.download(config.xdg_cache_home + sessionId + "/chunk-stream" + req.params.streamId + "-" + utils.pad(parseInt(req.params.partId) + 1, 5) + ".m4s");
-            } else {
-                if (count < 20) {
-                    count++;
-                    setTimeout(doWork, 1000);
-                } else {
-                    res.status(404).send('Not found');
-                }
-            }
-        }
-    }
-    doWork();
-
     if ((typeof universal.cache[sessionId]) != 'undefined' && universal.cache[sessionId].alive == true) {
-        universal.updateTimeout(sessionId);
+        universal.cache[sessionId].getChunk(parseInt(req.params.partId), (chunkId) => {
+            debug('Serving chunk-stream' + req.params.streamId + "-" + utils.pad(chunkId + 1, 5) + '.m4s for session ' + sessionId);
+            res.sendFile(config.xdg_cache_home + sessionId + "/chunk-stream" + req.params.streamId + "-" + utils.pad(chunkId + 1, 5) + ".m4s");
+
+            universal.updateTimeout(sessionId);
+        }, req.params.streamId)
+    } else {
+        debug(req.params.sessionId + ' not found');
+        res.status(404).send('Session not found');
     }
 };
 
