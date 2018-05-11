9d9dfcd1161bb889d6197f700e244ea552d5b118
Benjamin DROUARD <benjamin.drouard@epitech.eu>
2018-05-11 18:04:00 +0100

    Rewrite Long Polling Including ranges

--- core/stream.js
+++ core/stream.js
@@ -3,129 +3,183 @@
  */
 
 const fs = require('fs');
-const debug = require('debug')('stream');
+const debug = require('debug')('Stream');
 const Transcoder = require('./transcoder');
 const config = require('../config');
 const universal = require('./universal');
 const utils = require('../utils/utils');
 
-let stream = {};
+class Stream {
+    static serve(req, res) {
+        let transcoder;
+        let sessionId = req.query.session.toString();
 
-stream.serve = function(req, res) {
-    debug('Stream ' + req.query.session.toString());
+        debug('serve ' + sessionId);
 
-    if (typeof universal.cache[req.query.session] == 'undefined') {
-        universal.cache[req.query.session] = new Transcoder(req.query.session, req);
+        if (typeof universal.cache[sessionId] === 'undefined') {
+            transcoder = universal.cache[sessionId] = new Transcoder(sessionId, req);
+        } else {
+            transcoder = universal.cache[sessionId];
+        }
+
+        universal.updateTimeout(sessionId);
+
+        if (typeof req.query['X-Plex-Session-Identifier'] !== 'undefined') {
+            universal.sessions[req.query['X-Plex-Session-Identifier']] = sessionId;
+        }
+
+        this.rangeParser(req);
+        this.serveHeader(req, res, transcoder, false);
     }
 
-    if (typeof req.query['X-Plex-Session-Identifier'] != 'undefined') {
-        universal.sessions[req.query['X-Plex-Session-Identifier']] = req.query.session.toString();
+    static serveSubtitles(req, res) {
+        let transcoder;
+        let sessionId = req.query.session.toString();
+
+        if (typeof universal.cache[req.query.session] === 'undefined') {
+            debug(" subtitle session " + sessionId + " not found");
+            res.status(404).send("Session not found");
+            return;
+        }
+
+        debug("serve subtitles " + sessionId);
+        transcoder = universal.cache[req.query.session];
+
+        this.serveHeader(req, res, transcoder, true);
     }
 
-    req.connection.on("close", stream.connectionClosed.bind(null, universal.cache[req.query.session]));
-    universal.cache[req.query.session].getChunk(0, stream.serveChunk.bind(null, req, res, universal.cache[req.query.session]))
-};
+    static rangeParser(req) {
+        let range = req.range(500 * (1024 * 1024 * 1024));
 
-stream.serveChunk = function (req, res, transcoder, chunkId) {
-    let cwd = config.xdg_cache_home + req.query.session + "/";
+        if (typeof range === "object") {
+            if (range.type !== "bytes")
+                debug("WARNING range type " + range.type);
 
-    function doWork() {
-        if (!req.connection.destroyed) {
-            universal.updateTimeout(req.query.session);
+            if (range.length > 0) {
+                req.parsedRange = range[0];
+                req.streamCursor = 0;
+            }
+        }
+    }
 
-            let chkPath = cwd + "chunk-" + utils.pad(chunkId, 5);
-            fs.access(chkPath, fs.constants.R_OK, (err) => {
-                if (err) {
-                    res.end();
+    static serveHeader(req, res, transcoder, isSubtitle) {
+        transcoder.getChunk(0, (chunkId) => {
+            switch (chunkId) {
+                case -1:
+                    this.endConnection(req, res, isSubtitle);
                     return;
-                }
 
-                let fileStream = fs.createReadStream(chkPath);
-                fileStream.pipe(res, {end: false});
-                fileStream.on('end', transcoder.getChunk.bind(transcoder, chunkId + 1, stream.serveChunk.bind(null, req, res, transcoder)))
-            });
-        }
+                case -2:
+                    this.serveHeader(req, res, transcoder, isSubtitle);
+                    return;
+
+                default:
+                    this.streamBuilder(req, res, isSubtitle, -1, () => {
+                        this.serveChunk(req, res, isSubtitle, 0);
+                    });
+            }
+        }, (isSubtitle ? 'sub' : '0'), true)
     }
-    if (chunkId == -2) {
-        transcoder.getChunk(chunkId, stream.serveSubtitleChunk.bind(null, req, res, transcoder))
-    } else if (chunkId == -1) {
-        if (!req.connection.destroyed) {
-            res.end();
-            debug(req.query.session + ' end');
-        }
-    } else if (chunkId == 0) {
-        stream.serveHeader(req, res, cwd, false, doWork);
+
+    static serveChunk(req, res, transcoder, isSubtitle, chunkId) {
+        transcoder.getChunk(chunkId, (chunkId) => {
+            switch (chunkId) {
+                case -1:
+                    this.endConnection(req, res, isSubtitle);
+                    return;
+
+                case -2:
+                    this.serveChunk(req, res, transcoder, isSubtitle, chunkId);
+                    return;
+
+                default:
+                    this.streamBuilder(req, res, isSubtitle, chunkId, () => {
+                        this.serveChunk(req, res, isSubtitle, chunkId + 1);
+                    });
+            }
+        }, (isSubtitle ? 'sub' : '0'), true)
     }
-    else
-        doWork()
-};
 
-stream.serveHeader = function (req, res, cwd, subtitle, callback) {
-    if (!req.connection.destroyed) {
-        debug('Serving ' + (subtitle ? ' subtitles ' :  '') + req.query.session);
-        res.type((subtitle ? config.subtitles_content_type : config.video_content_type));
+    static streamBuilder(req, res, transcoder, isSubtitle, chunkId, callback) {
+        //Build the chunk Path
+        let chunkPath = config.xdg_cache_home + req.query.session + "/" + (isSubtitle ? 'sub-' : '') + (chunkId === -1 ? 'header' : 'chunk-' + utils.pad(chunkId, 5));
 
-        let chkPath = cwd + (subtitle ? 'sub-' : '') + 'header';
-        fs.access(chkPath, fs.constants.R_OK, (err) => {
+        //Access the file to get the size
+        fs.stat(chunkPath, (err, stats) => {
             if (err) {
-                res.end();
+                this.endConnection(req, res, isSubtitle);
                 return;
             }
 
-            let fileStream = fs.createReadStream(chkPath);
-            fileStream.pipe(res, {end: false});
-            fileStream.on('end', callback);
-        });
-    }
-};
+            let fileStream;
+            let sizeToRead;
 
+            //Check if we have to skip some data
+            if (typeof req.parsedRange !== "undefined" && typeof req.parsedRange.start === "number" && (req.parsedRange.start - req.streamCursor) > 0) {
+                let toSkip = req.parsedRange.start - req.streamCursor;
 
-stream.serveSubtitles = function (req, res) {
-    if (typeof universal.cache[req.query.session] != 'undefined') {
-        debug('Subtitles ' + req.query.session.toString());
-        universal.cache[req.query.session].getChunk(0, stream.serveSubtitleChunk.bind(null, req, res, universal.cache[req.query.session]), 'sub')
-    } else {
-        res.status(404).send('Invalid session')
-    }
-};
-
-stream.serveSubtitleChunk = function (req, res, transcoder, chunkId) {
-    let cwd = config.xdg_cache_home + req.query.session + "/";
-
-    function doWork() {
-        if (!req.connection.destroyed) {
-            universal.updateTimeout(req.query.session);
-
-            let chkPath = cwd + "sub-chunk-" + utils.pad(chunkId, 5);
-            fs.access(chkPath, fs.constants.R_OK, (err) => {
-                if (err) {
-                    res.end();
+                //Skip the whole file
+                if (toSkip > stats.size) {
+                    req.streamCursor += stats.size;
+                    callback();
                     return;
                 }
 
-                let fileStream = fs.createReadStream(chkPath);
-                fileStream.pipe(res, {end: false});
-                fileStream.on('end', transcoder.getChunk.bind(transcoder, chunkId + 1, stream.serveSubtitleChunk.bind(null, req, res, transcoder), 'sub'))
-            });
-        }
+                //Skip only n bytes
+                fileStream = fs.createReadStream(chunkPath, {
+                    start: toSkip
+                });
+                req.streamCursor += toSkip;
+                sizeToRead = stats.size - toSkip;
+            } else {
+                sizeToRead = stats.size;
+                fileStream = fs.createReadStream(chunkPath);
+            }
+
+            //Check the end range
+            if (typeof req.parsedRange !== "undefined" && typeof req.parsedRange.end === "number" && req.parsedRange.end < (req.streamCursor + sizeToRead)) {
+                //Extract data and push it to the stream
+                fileStream.on('readable', () => {
+                    let buffer;
+                    while (null !== (buffer = fileStream.read(req.parsedRange.end - req.streamCursor))) {
+                        if (req.connection.destroyed) {
+                            fileStream.removeAllListeners('readable');
+                            return;
+                        }
+
+                        res.write(buffer, 'binary');
+
+                        req.streamCursor += buffer.length;
+                        if (req.streamCursor >= req.parsedRange.end) {
+                            fileStream.removeAllListeners('readable');
+                            this.endConnection(req, res, isSubtitle);
+                            return;
+                        }
+                    }
+                });
+            } else {
+                //Send the whole file
+                if (!req.connection.destroyed) {
+                    fileStream.pipe(res, {end: false});
+                    fileStream.on('end', () => {
+                        req.streamCursor += sizeToRead;
+
+                        if (typeof req.parsedRange !== "undefined" && typeof req.parsedRange.end === "number" && req.parsedRange.end === req.streamCursor)
+                            this.endConnection(req, res, isSubtitle);
+                        else
+                            callback();
+                    });
+                }
+            }
+        });
     }
-    if (chunkId == -2) {
-        transcoder.getChunk(chunkId, stream.serveSubtitleChunk.bind(null, req, res, transcoder), 'sub')
-    } else if (chunkId == -1) {
+
+    static endConnection(req, res, isSubtitle) {
         if (!req.connection.destroyed) {
             res.end();
-            debug(req.query.session + ' subtitles end');
+            debug(req.query.session + (isSubtitle ? ' subtitles' : '') + ' end');
         }
-    } else if (chunkId == 0) {
-        stream.serveHeader(req, res, cwd, true, doWork);
     }
-    else
-        doWork()
-};
+}
 
-stream.connectionClosed = function (transcoder) {
-    debug(transcoder.sessionId + ' connection closed');
-    //transcoder.killInstance()
-};
-
-module.exports = stream;
+module.exports = Stream;
