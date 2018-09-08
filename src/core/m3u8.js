dc386a6927a94fafee9727129218bc1fc6c4badd
Matthew Knox <matthew@makereti.co.nz>
2018-09-08 19:02:28 +1000

    Combine logic from m3u8"

--- src/core/m3u8.js
+++ src/core/m3u8.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
 const path = require('path');
+const { fileExists } = require('../utils/files');
 const Transcoder = require('./transcoder');
 const sleep = require('../utils/sleep');
 
@@ -8,12 +8,12 @@ class M3U8 {
         this._config = config;
         this._ws = websocket;
         this._universal = universal;
-        this._plexCachePath = path.join(this._config.plex.transcoder, "Cache");
-        this._plexResourcesPath = path.join(this._config.plex.transcoder, "Resources");
+        this._plexCachePath = path.join(this._config.plex.transcoder, 'Cache');
+        this._plexResourcesPath = path.join(this._config.plex.transcoder, 'Resources');
     }
 
-    async serve(req, res) {
-        console.log('M3U8 ' + req.params.sessionId);
+    async serve(req) {
+        console.log(`M3U8 ${req.params.sessionId}`);
         if (this._universal.getCache(req.params.sessionId) !== void (0)) {
             await this._universal.getCache(req.params.sessionId).killInstance();
         }
@@ -22,65 +22,48 @@ class M3U8 {
     }
 
     async serveChunk(req, res) {
-        let sessionId = req.params.sessionId;
-        console.log(`Requesting ${req.params.partId} for session ${sessionId}`);
-
-        const tr = this._universal.getCache(sessionId);
-        if (tr !== void(0) && tr.alive === true) {
-            const chunkId = await tr.getChunk(req.params.partId);
-            const file = path.join(this._plexCachePath, sessionId, `media-${req.params.partId}.ts`);
-            if (chunkId == -2) {
-                if (!res.headersSent) {
-                    res.status(404).send(`Callback ${chunkId}`);
-                }
-            } else if (chunkId == -1 && !fs.existsSync(file)) {
-                console.log(`Serving fake ${req.params.partId} for session ${sessionId}`);
-                res.sendFile(path.join(this._plexResourcesPath, 'Resources', 'empty.ts'));
-            } else {
-                console.log(`Serving ${req.params.partId} for session ${sessionId}`);
-                res.sendFile(file);
-            }
-            await this._universal.updateTimeout(sessionId);
-        } else {
-            console.log(`${sessionId} not found`);
-
-            this._universal.putCache(sessionId, new Transcoder(this._config, this._ws, this._universal, sessionId));
-            await this._universal.updateTimeout(sessionId);
-
-            await sleep(10000);
-            res.status(404).send('Restarting session');
-        }
+        return await this._serveCommon(req, res);
     }
 
     async serveSubtitles(req, res) {
-        let sessionId = req.params.sessionId;
-        console.log(`Requesting subtitles ${req.params.partId} for session ${sessionId}`);
+        return await this._serveCommon(req, res, 'sub');
+    }
+
+    async _serveCommon(req, res, type = void(0)) {
+        const sessionId = req.params.sessionId;
+        const logType = type === 'sub' ? 'subtitles' : 'stream';
+        const extension = type === 'sub' ? 'vtt' : 'ts';
+        console.log(`Requesting ${logType} ${req.params.partId} for session ${sessionId}`);
 
         const tr = this._universal.getCache(sessionId);
-        if (tr !== void(0) && tr.alive === true) {
-            const chunkId = await tr.getChunk(req.params.partId, 'sub');
-            const file = path.join(this._plexCachePath, sessionId, `media-${req.params.partId}.vtt`);
-            if (chunkId == -2) {
-                if (!res.headersSent) {
-                    res.status(404).send(`Callback ${chunkId}`);
-                }
-            } else if (chunkId == -1 && !fs.existsSync(file)) {
-                console.log(`Serving fake subtitles ${req.params.partId} for session ${sessionId}`);
-                res.sendFile(path.join(this._plexResourcesPath, 'Resources', 'empty.vtt'));
-            } else {
-                console.log(`Serving subtitles ${req.params.partId} for session ${sessionId}`);
-                res.sendFile(file);
-            }
-            await this._universal.updateTimeout(sessionId);
-        } else {
-            console.log(sessionId + ' not found');
-
+        if (tr === void (0) || tr.alive === false) {
+            console.log(`${sessionId} not found`);
             this._universal.putCache(sessionId, new Transcoder(this._config, this._ws, this._universal, sessionId));
             await this._universal.updateTimeout(sessionId);
-
             await sleep(10000);
             res.status(404).send('Restarting session');
+            return;
         }
+
+        const chunkId = await tr.getChunk(req.params.partId, type);
+        const file = path.join(this._plexCachePath, sessionId, `media-${req.params.partId}.${extension}`);
+        if (chunkId == -2) {
+            if (!res.headersSent) {
+                res.status(404).send(`Callback ${chunkId}`);
+            }
+            else {
+                res.status(200);
+            }
+        }
+        else if (chunkId == -1 && !(await fileExists(file))) {
+            console.log(`Serving fake ${logType} ${req.params.partId} for session ${sessionId}`);
+            res.sendFile(path.join(this._plexResourcesPath, 'Resources', `empty.${extension}`));
+        }
+        else {
+            console.log(`Serving ${logType} ${req.params.partId} for session ${sessionId}`);
+            res.sendFile(file);
+        }
+        await this._universal.updateTimeout(sessionId);
     }
 
     saveSession(req) {
