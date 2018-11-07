f4fa334060b92d11523c1522edbe822266cc8def
Benjamin DROUARD <benjamin.drouard@epitech.eu>
2018-11-07 18:58:19 +0100

    Rework config and auto-setup with grunt

---
+++ Gruntfile.js
@@ -0,0 +1,110 @@
+const ar = require('ar');
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+const config = require('./config');
+
+module.exports = (grunt) => {
+    let codecsFolder = config.transcoder.codecs_folder + config.transcoder.codecs_build + '-linux-ubuntu-x86_64/';
+    let plexFolder = config.transcoder.plex_resources + config.transcoder.plex_build + '/';
+
+    let gruntConfig = {
+        'mkdir': {
+            codecs: {
+                options: {
+                    create: [codecsFolder]
+                },
+            },
+            plex: {
+                options: {
+                    create: [plexFolder]
+                }
+            }
+        },
+        'request-progress': {
+            plex: {
+                options: {
+                    allowOverwrite: true,
+                    dst: plexFolder + 'plex.deb',
+                    src: `https://downloads.plex.tv/plex-media-server/${config.transcoder.plex_build}/plexmediaserver_${config.transcoder.plex_build}_amd64.deb`
+                }
+            }
+        },
+        'arx': {
+            plex: {
+                src: plexFolder + "plex.deb",
+                dst: plexFolder
+            }
+        },
+        'untar': {
+            plex: {
+                options: {
+                    mode: 'tgz'
+                },
+                files: {
+                    [plexFolder]: plexFolder + 'data.tar.gz',
+                }
+            }
+        }
+    };
+
+    //Generate codecs downloads
+    // for f in *; do sed "s/\(.*\).so/'\1',/" <<< "$f"; done
+    const codecs = [
+        'libaac_decoder',
+        'libaac_encoder',
+        'libac3_decoder',
+        'libac3_encoder',
+        'libcook_decoder',
+        'libdca_decoder',
+        'libflv_decoder',
+        'libh264_decoder',
+        'libhevc_decoder',
+        'liblibmp3lame_encoder',
+        'liblibx264_encoder',
+        'libmp2_decoder',
+        'libmp3_decoder',
+        'libmpeg2video_decoder',
+        'libmpeg4_decoder',
+        'libmsmpeg4v3_decoder',
+        'librv30_decoder',
+        'librv40_decoder',
+        'libvc1_decoder',
+        'libvp8_decoder',
+        'libvp9_decoder',
+        'libwmav2_decoder',
+        'libwmv2_decoder',
+    ];
+    const codecTaskSample = {
+            dst: codecsFolder,
+            src: 'https://downloads.plex.tv/codecs/%s/linux-ubuntu-x86_64/%s.so',
+            allowOverwrite: true
+    };
+    codecs.forEach((codec) => {
+        let codecTask = { options: {...codecTaskSample}, };
+        codecTask.options.src = util.format(codecTask.options.src, config.transcoder.codecs_build, codec);
+        codecTask.options.dst = codecTask.options.dst + codec + ".so";
+        gruntConfig["request-progress"][codec] = codecTask;
+    });
+    let codecsTask = codecs.map(c => 'request-progress:' + c);
+    codecsTask.unshift('mkdir:codecs');
+
+    //The deb extraction task
+    grunt.registerMultiTask('arx', 'Extract a GNU ar archive', function() {
+        let archive = new ar.Archive(fs.readFileSync(this.data.src));
+        let files = archive.getFiles();
+        files.forEach((file) => {
+            grunt.file.write(path.resolve(this.data.dst, file.name()), file.fileData());
+        });
+    });
+
+    grunt.initConfig(gruntConfig);
+    grunt.loadNpmTasks('grunt-request-progress');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-untar');
+
+    grunt.registerTask('plex', ['mkdir:plex', 'request-progress:plex', 'arx:plex', 'untar:plex']);
+    grunt.registerTask('codecs', codecsTask);
+    grunt.registerTask('default', ['plex', 'codecs']);
+
+};
--- config.js
+++ config.js
@@ -2,45 +2,20 @@
  * Created by drouar_b on 27/04/2017.
  */
 
-const getenv = require('getenv');
+const env = require('getenv');
 
-module.exports = getenv.multi({
-    // app config
-    port:                  ["PORT", 3000, 'int'],
-    mount_point:           ['MOUNT_POINT', '/mnt/acd'],
-    transcoder_decay_time: ['TRANSCODER_DECAY_TIME', 120, 'int'],
+module.exports = {
+    port:                  env.int   ('SERVER_PORT',           3000),
+    host:                  env.string('SERVER_LISTEN',         '127.0.0.1'),
+    mount_point:           env.string('MOUNT_POINT',           '/media'),
+    transcoder_decay_time: env.int   ('TRANSCODER_DECAY_TIME', 120),
+    loadbalancer_address:  env.url   ('LOADBALANCER_ADDRESS',  'https://unicornloadbalancer.myplex.com'),
 
-    //plex config
-    plex_url: ['PLEX_URL', 'http://myplex.com:32400'],
-    base_url: ['BASE_URL', 'https://myplex.com'],
-
-    //redis config
-    redis_host: ['REDIS_HOST', '127.0.0.1'],
-    redis_port: ['REDIS_PORT', '6379'],
-    redis_pass: ['REDIS_PASSWORD', ''],
-    redis_db:   ['REDIS_DB', 1, 'int'],
-
-    //Transcoder settings
-    video_content_type:       ['VIDEO_CONTENT_TYPE', 'video/x-matroska'],
-    subtitles_content_type:   ['SUBTITLES_CONTENT_TYPE', 'text/vtt'],
-    ld_library_path:          ['LD_LIBRARY_PATH', '/root/unicorn/UnicornTranscoder/Resources/'],
-    transcoder_path:          ['TRANSCODER_PATH', '/root/unicorn/UnicornTranscoder/Resources/Plex Transcoder'],
-    ffmpeg_external_libs:     ['FFMPEG_EXTERNAL_LIBS', '/root/unicorn/UnicornTranscoder/Codecs/'],
-    eae_root:                 ['EAE_ROOT', '/root/unicorn/UnicornTranscoder/Cache/'],
-    xdg_cache_home:           ['XDG_CACHE_HOME', '/root/unicorn/UnicornTranscoder/Cache/'],
-    xdg_data_home:            ['XDG_DATA_HOME', '/root/unicorn/UnicornTranscoder/Resources/Resources/'],
-    plex_ressources:          ['PLEX_RESSOURCES', '/root/unicorn/UnicornTranscoder/Resources/']
-});
-
-// Public configuration
-module.exports.public_config = getenv.multi({
-	serverName:               ["SERVER_NAME", ''],
-	preferredMaxSessions:     ['MAX_SESSIONS', 10, 'int'],
-	preferredMaxDownloads:    ['MAX_DOWNLOADS', 10, 'int'],
-	preferredMaxTranscodes:   ['MAX_TRANSCODES', 10, 'int']
-});
-
-//Allow to reroute via another URL from the country code
-module.exports.routing = {
-    'FR': 'https://fr.mytranscoder.myplex.com'
+    transcoder: {
+        plex_build:        env.string('PLEX_BUILD',            '1.13.8.5395-10d48da0d'),
+        codecs_build:      env.string('CODECS_BUILD',          'e7828f1-1324'),
+        plex_resources:    env.string('PLEX_RESOURCES',        'plexmediaserver/'),
+        temp_folder:       env.string('TEMP_FOLDER',           'cache/'),
+        codecs_folder:     env.string('CODECS_FOLDER',         'codecs/')
+    }
 };
