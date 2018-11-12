d25f7eeda52c850f7df96530918bcf3cbdf53f4a
Benjamin DROUARD <benjamin.drouard@epitech.eu>
2018-11-12 17:48:05 +0100

    Fix killInstace

--- core/session-manager.js
+++ core/session-manager.js
@@ -54,7 +54,7 @@ class SessionManager {
         if (sessionId in this.transcoderStore && this.transcoderStore[sessionId].alive) {
             if (typeof  this.transcoderStore[sessionId].sessionTimeout !== 'undefined')
                 clearTimeout(this.transcoderStore[sessionId].sessionTimeout);
-            this.transcoderStore[sessionId].killInstance(true, () => {
+            this.transcoderStore[sessionId].killInstance(() => {
                 delete this.transcoderStore[sessionId];
                 callback();
             })
