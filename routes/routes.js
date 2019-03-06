c3d9c946986f623539978bae2d86453cc3145f45
Maxime Baconnais <m.baconnais@outlook.com>
2019-03-06 21:14:06 +0100

    Patch routes to support music endpoints

--- routes/routes.js
+++ routes/routes.js
@@ -16,27 +16,27 @@ const progress = require('../core/progress');
 const SessionManager = require('../core/session-manager');
 
 //Dash routes
-router.get('/video/:/transcode/universal/start.mpd', dash.serve);
-router.get('/video/:/transcode/universal/dash/:sessionId/:streamId/initial.mp4', dash.serveInit);
-router.get('/video/:/transcode/universal/dash/:sessionId/:streamId/:partId.m4s', dash.serveChunk);
+router.get('/:formatType/:/transcode/universal/start.mpd', dash.serve);
+router.get('/:formatType/:/transcode/universal/dash/:sessionId/:streamId/initial.mp4', dash.serveInit);
+router.get('/:formatType/:/transcode/universal/dash/:sessionId/:streamId/:partId.m4s', dash.serveChunk);
 
 //m3u8 mode
-router.get('/video/:/transcode/universal/session/:sessionId/base/index.m3u8', m3u8.serve);
-router.get('/video/:/transcode/universal/session/:sessionId/base-x-mc/index.m3u8', m3u8.serve);
-router.get('/video/:/transcode/universal/session/:sessionId/vtt-base/index.m3u8', proxy);
-router.get('/video/:/transcode/universal/session/:sessionId/:fileType/:partId.ts', m3u8.serveChunk);
-router.get('/video/:/transcode/universal/session/:sessionId/:fileType/:partId.vtt', m3u8.serveSubtitles);
+router.get('/:formatType/:/transcode/universal/session/:sessionId/base/index.m3u8', m3u8.serve);
+router.get('/:formatType/:/transcode/universal/session/:sessionId/base-x-mc/index.m3u8', m3u8.serve);
+router.get('/:formatType/:/transcode/universal/session/:sessionId/vtt-base/index.m3u8', proxy);
+router.get('/:formatType/:/transcode/universal/session/:sessionId/:fileType/:partId.ts', m3u8.serveChunk);
+router.get('/:formatType/:/transcode/universal/session/:sessionId/:fileType/:partId.vtt', m3u8.serveSubtitles);
 
 //Stream mode
-router.get('/video/:/transcode/universal/start', stream.serve);
-router.get('/video/:/transcode/universal/subtitles', stream.serveSubtitles);
+router.get('/:formatType/:/transcode/universal/start', stream.serve);
+router.get('/:formatType/:/transcode/universal/subtitles', stream.serveSubtitles);
 
 // Download files
 router.get('/library/parts/:id1/:id2/file.*', download.serve);
 
 //Transcoder progression
-router.post('/video/:/transcode/session/:sessionId/:uuid/seglist', bodyParser.text({ type: () => {return true}, limit: '50mb' }), ffmpeg.seglistParser);
-router.post('/video/:/transcode/session/:sessionId/:uuid/manifest', bodyParser.text({ type: () => {return true}, limit: '50mb' }), ffmpeg.manifestParser);
+router.post('/:formatType/:/transcode/session/:sessionId/:uuid/seglist', bodyParser.text({ type: () => {return true}, limit: '50mb' }), ffmpeg.seglistParser);
+router.post('/:formatType/:/transcode/session/:sessionId/:uuid/manifest', bodyParser.text({ type: () => {return true}, limit: '50mb' }), ffmpeg.manifestParser);
 
 //UnicornTranscoder API
 router.get('/api/sessions', SessionManager.stats.bind(SessionManager));
@@ -45,8 +45,8 @@ router.get('/api/stop', SessionManager.stopTranscoder.bind(SessionManager));
 router.get('/api/ping', SessionManager.ping.bind(SessionManager));
 
 //Plex Progress URL
-router.all('/video/:/transcode/session/:sessionId/:uuid/progress', bodyParser.text({ type: () => {return true}, limit: '50mb' }), progress.progress);
-router.all('/video/:/transcode/session/:sessionId/:uuid/progress/*', bodyParser.text({ type: () => {return true}, limit: '50mb' }), progress.progress);
+router.all('/:formatType/:/transcode/session/:sessionId/:uuid/progress', bodyParser.text({ type: () => {return true}, limit: '50mb' }), progress.progress);
+router.all('/:formatType/:/transcode/session/:sessionId/:uuid/progress/*', bodyParser.text({ type: () => {return true}, limit: '50mb' }), progress.progress);
 router.all('/log', bodyParser.text({ type: () => {return true}, limit: '50mb' }), progress.log);
 
 module.exports = router;
