1412256177f2e980af433db7a82176c7e462860f
Maxime Baconnais <m.baconnais@outlook.com>
2019-03-25 23:19:03 +0100

    Resolver support (Not tested)

--- core/download.js
+++ core/download.js
@@ -5,21 +5,37 @@
 const debug = require('debug')('UnicornTranscoder:Download');
 const path = require('path');
 const request = require('request');
+const httpProxy = require('http-proxy');
 const config = require('../config');
 const SessionManager = require('./session-manager');
 
 class Download {
     static serve(req, res) {
-        request(config.loadbalancer_address + '/api/path/' + req.params.id1, (error, response, body) => {
+        request(config.loadbalancer_address + '/api/path_v2/' + req.params.id1, (error, response, body) => {
             if (!error && response.statusCode === 200) {
-                let result = JSON.parse(body);
-                if (typeof result.file === "undefined")
-                    return res.status(404).send('404 File not found');
-                debug(result.file);
-                SessionManager.startDownload(result.file);
-                res.download(result.file, path.basename(result.file), () => {
-                    SessionManager.stopDownload(result.file);
-                })
+                const file = JSON.parse(body);
+                // Local file, serve
+                if (file.type === 'LOCAL') {
+                    SessionManager.startDownload(file.path);
+                    res.download(file.path, path.basename(file.path), () => {
+                        SessionManager.stopDownload(file.path);
+                    })
+                }
+                // 302 to download link
+                else if (file.type === 'URL' && file.direct) {
+                    res.redirect(302, file.path);
+                    debug('REDIRECT ' + file.path);
+                }
+                // Proxy file
+                else if (file.type === 'URL' && !file.direct) {
+                    const proxy = httpProxy.createProxyServer({
+                        target: file.path,
+                        ignorePath: true
+                    }).on('error', (err) => {
+                        return res.status(404).send('404 File not found')
+                    })
+                    return proxy.web(req, res);
+                }
             } else {
                 res.status(404).send('404 File not found')
             }
