27fc1d29b11b66f5aaded4ade4607cb178e39084
Maxime Baconnais <m.baconnais@outlook.com>
2020-02-02 19:04:27 +0100

    update plex version

--- config.js
+++ config.js
@@ -16,9 +16,9 @@ module.exports = {
 
     transcoder: {
         plex_arch:         env.string('PLEX_ARCH',             'amd64'), // Can be: amd64, arm64, armhf, i386
-        plex_build:        env.string('PLEX_BUILD',            '1.18.0.1944-f2cae8d6b'),
-        codecs_build:      env.string('CODECS_BUILD',          '20fed83-2561'),
-        eae_version:       env.string('EAE_VERSION',           'eae-69c1de6-41'),
+        plex_build:        env.string('PLEX_BUILD',            '1.18.5.2309-f5213a238'),
+        codecs_build:      env.string('CODECS_BUILD',          '8bf330d-2818'),
+        eae_version:       env.string('EAE_VERSION',           'eae-69c1de6-42'), // 41 or 42?
         plex_resources:    env.string('PLEX_RESOURCES',        'plexmediaserver/'),
         temp_folder:       env.string('TEMP_FOLDER',           'cache/'),
         codecs_folder:     env.string('CODECS_FOLDER',         'codecs/'),
